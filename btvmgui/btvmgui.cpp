dbbce03499c24b38239bbec6048c283640c7a6d9
jeppeter <jeppeter@gmail.com>
2015-10-22 17:09:21 +0800

    to add icon to btvmgui

--- btvmgui/btvmgui.cpp
+++ btvmgui/btvmgui.cpp
@@ -21,5 +21,18 @@ CControlUI* Cbtvmgui::CreateControl( LPCTSTR pstrClassName )
 
 void Cbtvmgui::InitWindow()
 {
+	/*now to set icon*/
+	HINSTANCE hinstance=NULL;
+	hinstance = CPaintManagerUI::GetInstance();
+	HANDLE hicon = NULL;
+	hicon = ::LoadImage(hinstance,_T("logo.ico"), IMAGE_ICON, 32, 32, LR_LOADFROMFILE);
+
+	if (hicon != NULL){
+		::MessageBox(this->GetHWND(),_T("Set Icon"),_T("Notice"),MB_OK);
+		::PostMessage(this->GetHWND(), (UINT)WM_SETICON, ICON_BIG, (LPARAM)hicon);
+	}else{
+		::MessageBox(this->GetHWND(),_T("Not Set Icon"),_T("Notice"),MB_OK);
+	}
+
     CenterWindow();
 }
