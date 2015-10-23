f1526808ba1e1e5be3bd6a7ee315e9f56909782d
jeppeter <jeppeter@gmail.com>
2015-10-23 10:19:09 +0800

    to make button exit

--- btvmgui/btvmgui.cpp
+++ btvmgui/btvmgui.cpp
@@ -10,6 +10,12 @@ Cbtvmgui::Cbtvmgui( LPCTSTR pszXMLPath )
 
 void Cbtvmgui::Notify( TNotifyUI& msg )
 {
+	if (msg.sType == _T("click")){
+		if (msg.pSender->GetName() == _T("ButtonExit")){
+			Close();
+			return ;
+		}
+	}
     __super::Notify(msg);
 }
 
@@ -31,6 +37,12 @@ void Cbtvmgui::InitWindow()
 		::PostMessage(this->GetHWND(), (UINT)WM_SETICON, ICON_BIG, (LPARAM)hicon);
 	}else{
 		::MessageBox(this->GetHWND(),_T("Not Set Icon"),_T("Notice"),MB_OK);
+	}
+
+	CButtonUI* phyper= NULL;
+	phyper = static_cast<CButtonUI*>(m_PaintManager.FindControl(_T("hyperlink")));
+	if (phyper){
+		phyper->SetText(_T("{u}{a}http://www.sina.com.cn{/a}{/u}"));
 	}
 
     CenterWindow();
