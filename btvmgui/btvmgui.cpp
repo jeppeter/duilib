b16811ab6b42a657393a93c2f78b9cb4a883ea82
jeppeter <jeppeter@gmail.com>
2015-10-24 11:20:26 +0800

    to make resource zip

--- btvmgui/btvmgui.cpp
+++ btvmgui/btvmgui.cpp
@@ -1,8 +1,10 @@
 
 #include "btvmgui.h"
 #include "output_debug.h"
+#include <gdiplus.h>
+#pragma comment(lib,"GdiPlus.lib")
 
-Cbtvmgui::Cbtvmgui( LPCTSTR pszXMLPath )
+Cbtvmgui::Cbtvmgui(LPCTSTR pszXMLPath)
     : Ctraydlg(pszXMLPath)
 {
     this->m_timeout = 3;
@@ -15,43 +17,43 @@ Cbtvmgui::~Cbtvmgui()
 }
 
 
-void Cbtvmgui::Notify( TNotifyUI& msg )
+void Cbtvmgui::Notify(TNotifyUI& msg)
 {
     TCHAR numstr[32];
     int timeout;
-    if (msg.sType == _T("click")) {
-        if (msg.pSender->GetName() == _T("ButtonExit")) {
+    if(msg.sType == _T("click")) {
+        if(msg.pSender->GetName() == _T("ButtonExit")) {
             Close();
             return ;
-        } else if (msg.pSender->GetName() == _T("closebtn")) {
+        } else if(msg.pSender->GetName() == _T("closebtn")) {
             this->TrayShow();
             this->ShowWindow(false,true);
             return ;
-        } else if (msg.pSender->GetName() == _T("Buttonup")) {
+        } else if(msg.pSender->GetName() == _T("Buttonup")) {
             CLabelUI* pLabel=static_cast<CLabelUI*>(m_PaintManager.FindControl(_T("LabelTimeout")));
-            if (pLabel) {
+            if(pLabel) {
                 timeout = this->m_timeout;
                 timeout ++;
-                if (timeout <= 30) {
+                if(timeout <= 30) {
                     this->m_timeout = timeout;
                 }
                 _sntprintf(numstr,sizeof(numstr),_T("%d"),this->m_timeout);
                 pLabel->SetText(numstr);
             }
             return ;
-        } else if (msg.pSender->GetName() == _T("Buttondown")) {
+        } else if(msg.pSender->GetName() == _T("Buttondown")) {
             CLabelUI* pLabel=static_cast<CLabelUI*>(m_PaintManager.FindControl(_T("LabelTimeout")));
-            if (pLabel) {
+            if(pLabel) {
                 timeout = this->m_timeout;
                 timeout --;
-                if (timeout >= 1) {
+                if(timeout >= 1) {
                     this->m_timeout = timeout;
                 }
                 _sntprintf(numstr,sizeof(numstr),_T("%d"),this->m_timeout);
                 pLabel->SetText(numstr);
             }
             return ;
-        } else if (msg.pSender->GetName() == _T("hyperlink")) {
+        } else if(msg.pSender->GetName() == _T("hyperlink")) {
             ::ShellExecute(NULL, _T("open"),this->m_hyperlink, NULL, NULL, SW_SHOWNORMAL);
         }
     }
@@ -59,7 +61,7 @@ void Cbtvmgui::Notify( TNotifyUI& msg )
 }
 
 
-CControlUI* Cbtvmgui::CreateControl( LPCTSTR pstrClassName )
+CControlUI* Cbtvmgui::CreateControl(LPCTSTR pstrClassName)
 {
     return NULL;
 }
@@ -70,26 +72,46 @@ void Cbtvmgui::InitWindow()
     /*now to set icon*/
     HINSTANCE hinstance=NULL;
     hinstance = CPaintManagerUI::GetInstance();
-    HANDLE hicon = NULL;
+    HICON hicon = NULL;
     TCHAR numstr[32];
+    TImageInfo* bitmap=NULL;
     CLabelUI* pLabel=static_cast<CLabelUI*>(m_PaintManager.FindControl(_T("LabelTimeout")));
-    if (pLabel) {
+    if(pLabel) {
         _sntprintf(numstr,sizeof(numstr),_T("%d"),this->m_timeout);
         pLabel->SetText(numstr);
     }
-    hicon = ::LoadImage(hinstance,_T("logo.ico"), IMAGE_ICON, 32, 32, LR_LOADFROMFILE);
 
-    if (hicon != NULL) {
-        ::PostMessage(this->GetHWND(), (UINT)WM_SETICON, ICON_BIG  , (LPARAM)hicon);
-        ::PostMessage(this->GetHWND(), (UINT)WM_SETICON, ICON_SMALL, (LPARAM)hicon);
-        this->TraySetIcon((HICON)hicon);
+    bitmap = CRenderEngine::LoadImage(_T("icon.png"),NULL,0);
+    if(bitmap) {
+        Gdiplus::Bitmap* pTmpBitmap=Gdiplus::Bitmap::FromHBITMAP(bitmap->hBitmap,NULL);
+        if(pTmpBitmap == NULL) {
+            goto failed;
+        }
+        hicon = NULL;
+        pTmpBitmap->GetHICON(&hicon);
+
+
+        if(hicon != NULL) {
+            ::PostMessage(this->GetHWND(), (UINT)WM_SETICON, ICON_BIG  , (LPARAM)hicon);
+            ::PostMessage(this->GetHWND(), (UINT)WM_SETICON, ICON_SMALL, (LPARAM)hicon);
+            this->TraySetIcon((HICON)hicon);
+        } else {
+            ::MessageBox(this->GetHWND(),_T("Not Set Icon"),_T("Notice"),MB_OK);
+        }
+failed:
+        if(pTmpBitmap) {
+            delete pTmpBitmap;
+        }
+        pTmpBitmap = NULL;
+        CRenderEngine::FreeImage(bitmap,true);
+        bitmap = NULL;
     } else {
-        ::MessageBox(this->GetHWND(),_T("Not Set Icon"),_T("Notice"),MB_OK);
+        ::MessageBox(this->GetHWND(),_T("Not Get Bitmap"),_T("Notice"),MB_OK);
     }
 
     CButtonUI* phyper= NULL;
     phyper = static_cast<CButtonUI*>(m_PaintManager.FindControl(_T("hyperlink")));
-    if (phyper) {
+    if(phyper) {
         phyper->SetText(_T("{u}{a}http://www.sina.com.cn{/a}{/u}"));
         _tcsncpy(this->m_hyperlink,_T("http://www.sina.com.cn"),sizeof(this->m_hyperlink)/sizeof(this->m_hyperlink[0]));
     }
